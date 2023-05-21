165015bffe45932315e810f2978e4220d3782404
ahmedhosam283843 <ahmed.hosam283843@gmail.com>
2023-05-21 10:23:50 +0300

    Initialize Backend

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const PORT = 5000;
+
+// enable CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // allow requests from any origin
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization" // add Authorization header here
+  ); // allow the headers that the client sends
+  next();
+});
+
+app.use(bodyParser.json());
+
+
+
+app.all("*", (req, res) =>
+  res.send("You've tried reaching a route that doesn't exist.")
+);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port:  http://localhost:${PORT}`);
+});
