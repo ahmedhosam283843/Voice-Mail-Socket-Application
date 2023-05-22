81aef7f1ddad3cda0cb49658f471ac896cb5075a
ahmedhosam283843 <ahmed.hosam283843@gmail.com>
2023-05-22 11:00:06 +0300

    feat: add db mail queries

---
+++ backend/db/db_queries/mail_queries.js
@@ -0,0 +1,37 @@
+import pool from "../db_pool.cjs";
+
+async function getMailByUserId(request, response) {
+  const id = parseInt(request.user);
+  pool.query(
+    "SELECT * FROM mail WHERE sender_id = $1",
+    [id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).json(results.rows);
+    }
+  );
+}
+
+async function createMail(request, response) {
+  const sender_id = parseInt(request.user);
+  const {
+    receiver_mail,
+    date_time,
+    audio_data,
+  } = request.body;
+  pool.query(
+    "INSERT INTO project (sender_id, receiver_mail, datetime, audio_data) VALUES ($1, $2, $3, $4)",
+    [sender_id, receiver_mail, date_time, audio_data],
+
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(201).send(`Mail created successfully`);
+    }
+  );
+}
+
+export { getMailByUserId, createMail };
