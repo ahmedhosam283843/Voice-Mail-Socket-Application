10633f1708fe6e1d4f280e5d2c9aa1760cf648ce
ahmedhosam283843 <ahmed.hosam283843@gmail.com>
2023-05-26 17:12:43 +0300

    fix: change module name

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import db_queries from "../db/db_queries/db_queries.js";
-import authenticateToken from "../middlewares/authenticateToken.js";
+import authenticateToken from "../middlewares/authenticate_token.js";
 
 const router = express.Router();
 
